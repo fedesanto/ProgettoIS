136b0dad4d9c2ae93c2a695bbd396a17d723652d
attil <attil@172.19.247.153>
2023-12-06 11:58:40 +0100

    Comment commit. Contiene: - eliminazione commenti superflui

--- src/main/java/progettoIS/gruppo01/Calculator/Managment/InputManagment.java
+++ src/main/java/progettoIS/gruppo01/Calculator/Managment/InputManagment.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package progettoIS.gruppo01.Calculator.Managment;
 
-/**
- *
- * @author attil
- */
+
 public interface InputManagment {
     
 }
--- src/main/java/progettoIS/gruppo01/Exceptions/MathException.java
+++ src/main/java/progettoIS/gruppo01/Exceptions/MathException.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
- */
 package progettoIS.gruppo01.Exceptions;
 
-/**
- *
- * @author attil
- */
+
 public class MathException extends Exception{
 
     /**
