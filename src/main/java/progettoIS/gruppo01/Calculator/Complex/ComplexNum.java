dc97bec9ccc88718dacbdf2fc7f84ce70ef9454d
attil <attil@LAPTOP-B5H566RC.homenet.telecomitalia.it>
2023-12-06 19:26:28 +0100

    ComplexNum, MathOperations commit. Sono state implementate le classi ComplexNum e MathOperations, bisogna aggiungere i commenti accanto

--- src/main/java/progettoIS/gruppo01/Calculator/Complex/ComplexNum.java
+++ src/main/java/progettoIS/gruppo01/Calculator/Complex/ComplexNum.java
@@ -1,6 +1,87 @@
 package progettoIS.gruppo01.Calculator.Complex;
 
-
 public class ComplexNum {
-    
+
+    private double realPart;
+    private double imgPart;
+
+    //Custruttore con parametri di inizializzazione
+    public ComplexNum(double realPart, double imgPart) {
+        this.realPart = realPart;
+        this.imgPart = imgPart;
+    }
+
+    public double getRealPart() {
+        return realPart;
+    }
+
+    public double getImgPart() {
+        return imgPart;
+    }
+
+    /*
+    Convertitore del numero complesso in stringa
+        Restituisce il numero nel formato “a + bj”, “a” o “bj”, 
+        dove a e b sono rispettivamente la parte reale ed immaginaria del numero
+     */
+    @Override
+    public String toString() {
+        if (this.imgPart == 0) {
+            return Double.toString(this.realPart);
+
+        } else if (this.realPart == 0) {
+            return Double.toString(this.imgPart) + 'j';
+
+        } else {
+
+            return Double.toString(this.realPart) + Double.toString(this.imgPart) + 'j';
+        }
+    }
+
+    /*
+    Costruzione di un numero complesso da una stringa nel formato “a + bj”, “a” o “bj”,
+    dove a e b sono rispettivamente la parte reale ed immaginaria del numero
+        
+        Input:
+            - num : numero complesso sotto forma di stringa
+        Output:
+            - numero complesso costruito dalla stringa oppure null
+            se il formato della stringa non è valido
+     */
+    public static ComplexNum parseComplex(String num) {
+        double real = 0;
+        double img = 0;
+
+        if (num.matches("-?\\d+(.\\d+)? [-+] \\d+(.\\d+)?j")) {
+            String numbers[] = num.split(" ");
+
+            real = Double.parseDouble(numbers[0]);
+
+            String sign = numbers[1];
+
+            String stringImg = numbers[2].substring(0, numbers[2].length() - 1);
+
+            if (sign.equals("-")) {
+                img = Double.parseDouble("-" + stringImg);
+            } else {
+                img = Double.parseDouble(stringImg);
+            }
+
+        } else if (num.matches("-?\\d+(.\\d+)?")) {
+            
+            real = Double.parseDouble(num);
+            
+        } else if (num.matches("-?\\d+(.\\d+)?j")) {
+            
+            String stringImg = num.substring(0, num.length() - 1);
+            
+            img = Double.parseDouble(stringImg);
+            
+        } else {
+            return null;
+        }
+
+        return new ComplexNum(real, img);
+    }
+
 }
--- src/main/java/progettoIS/gruppo01/Calculator/Complex/MathOperations.java
+++ src/main/java/progettoIS/gruppo01/Calculator/Complex/MathOperations.java
@@ -2,5 +2,161 @@ package progettoIS.gruppo01.Calculator.Complex;
 
 
 public class MathOperations {
+
+    /*
+    Metodo che gestisce la somma tra numeri complessi.
+    Il numero complesso risultato presenta come parte reale la somma tra
+    la parte reale del primo numero complesso e la parte reale del secondo,
+    invece, come parte immaginaria la somma tra la parte immaginaria del primo 
+    numero complesso e la parte immaginaria del secondo
     
+        Input:
+            - num1, primo numero complesso
+            - num2, secondo numero complesso
+        Output:
+            - numero complesso risultante dalla somma
+    */
+    public static ComplexNum somma(ComplexNum num1, ComplexNum num2) {
+        double sommaRealPart;
+        double sommaImgPart;
+
+        sommaRealPart = num1.getRealPart() + num2.getRealPart();
+
+        sommaImgPart = num1.getImgPart() + num2.getImgPart();
+
+        return new ComplexNum(sommaRealPart, sommaImgPart);
+    }
+
+    /*
+    Metodo che gestisce la sottrazione tra numeri complessi.
+    Il numero complesso risultato presenta come parte reale la sottrazione tra
+    la parte reale del primo numero complesso e la parte reale del secondo,
+    invece, come parte immaginaria la sottrazione tra la parte immaginaria del 
+    primo numero complesso e la parte immaginaria del secondo
+    
+        Input:
+            - num1, primo numero complesso
+            - num2, secondo numero complesso
+        Output:
+            - numero complesso risultante dalla sottrazione
+    */
+    public static ComplexNum sottrazione(ComplexNum num1, ComplexNum num2) {
+        double sottrRealPart;
+        double sottrImgPart;
+
+        sottrRealPart = num1.getRealPart() - num2.getRealPart();
+
+        sottrImgPart = num1.getImgPart() - num2.getImgPart();
+
+        return new ComplexNum(sottrRealPart, sottrImgPart);
+    }
+
+    /*
+    Metodo che gestisce la divisione tra numeri complessi.
+    Intendendo il primo numero complesso come "a + bj" e il secondo come 
+    "c +dj", dove a,b,c,d sono numeri reali, la divisione è data dalla formula:
+    (ac + bd) / (c^2 + d^2) + j * (bc - ad) / (c^2 + d^2)
+    
+        Input:
+            - num1, primo numero complesso
+            - num2, secondo numero complesso
+        Output:
+            - numero complesso risultante dalla divisione oppure null se num2 
+              è uguale a 0
+    */
+    public static ComplexNum divisione(ComplexNum num1, ComplexNum num2) {
+
+        if (num2.getRealPart() == 0 && num2.getImgPart() == 0) {
+            return null;
+        }
+
+        double a = num1.getRealPart();
+        double b = num1.getImgPart();
+        double c = num2.getRealPart();
+        double d = num2.getImgPart();
+
+        double divRealPart;
+        double divImgPart;
+        double denominatore = c * c + d * d;
+
+        divRealPart = (a * c + b * d) / denominatore;
+
+        divImgPart = (b * c - a * d) / denominatore;
+
+        return new ComplexNum(divRealPart, divImgPart);
+    }
+
+    /*
+    Metodo che gestisce la moltiplicazione tra numeri complessi.
+    Intendendo il primo numero complesso come "a + bj" e il secondo come 
+    "c +dj", dove a,b,c,d sono numeri reali, la moltiplicazione è data dalla formula:
+     (ac - bd) + j * (ad + bc)
+    
+        Input:
+            - num1, primo numero complesso
+            - num2, secondo numero complesso
+        Output:
+            - numero complesso risultante dalla moltiplicazione
+    */
+    public static ComplexNum moltiplicazione(ComplexNum num1, ComplexNum num2) {
+
+        double a = num1.getRealPart();
+        double b = num1.getImgPart();
+        double c = num2.getRealPart();
+        double d = num2.getImgPart();
+
+        double mulRealPart;
+        double mulImgPart;
+
+        mulRealPart = a * c - b * d;
+
+        mulImgPart = a * d + b * c;
+
+        return new ComplexNum(mulRealPart, mulImgPart);
+    }
+
+    /*
+    Metodo che gestisce la radice quadrata di un numero complesso.
+    Per un numero complesso z = a + bj, dove a e b sono numeri reali
+    e con modulo espresso come |z|, la radice quadrata è data dalla formula:
+     sqrt((|z| + a) / 2) + j * (sign(b) * sqrt((|z| - a) / 2))
+    
+        Input:
+            - num, numero complesso su cui effettuare la radice
+        Output:
+            - numero complesso risultante dalla radice quadrata (restituisce
+              solo il risultato della radice canonica)
+    */
+    public static ComplexNum sqrt(ComplexNum num) {
+
+        double sqrtrealPart;
+        double sqrtImgPart;
+
+        double a = num.getRealPart();
+        double b = num.getImgPart();
+
+        double module = Math.sqrt(a * a + b * b);
+
+        sqrtrealPart = Math.sqrt((double) (module + a) / 2);
+        sqrtImgPart = Math.sqrt((double) (module - a) / 2) * Math.signum(b);
+
+        return new ComplexNum(sqrtrealPart, sqrtImgPart);
+    }
+
+    /*
+    Metodo che gestisce l'inversione di segno di un numero complesso.
+    
+        Input:
+            - num, numero complesso su cui effettuare l'inversione di segno
+        Output:
+            - numero complesso risultante dall'inversione di segno
+    */
+    public static ComplexNum invSegno(ComplexNum num) {
+
+        double invrealPart = - num.getRealPart();
+        double invImgPart = - num.getImgPart();
+
+        return new ComplexNum(invrealPart, invImgPart);
+    }
+
 }
