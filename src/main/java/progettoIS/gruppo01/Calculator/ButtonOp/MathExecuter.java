d8076e16ac3216d52fa1b73386e51ab48cbaa5c1
miche <miche@LAPTOP-PB06LJB5>
2023-12-07 00:09:31 +0100

    Ho implementato i metodi,commentandoli, nella classe MathExecuter e aggiunto nuovi commenti nella classe StackCommander

--- src/main/java/progettoIS/gruppo01/Calculator/ButtonOp/MathExecuter.java
+++ src/main/java/progettoIS/gruppo01/Calculator/ButtonOp/MathExecuter.java
@@ -1,7 +1,10 @@
 package progettoIS.gruppo01.Calculator.ButtonOp;
 
+import progettoIS.gruppo01.Calculator.Complex.ComplexNum;
+import static progettoIS.gruppo01.Calculator.Complex.MathOperations.*;
 import progettoIS.gruppo01.Calculator.Structures.ComplexStack;
 import progettoIS.gruppo01.Exceptions.EmptyStackException;
+import progettoIS.gruppo01.Exceptions.FullStackException;
 import progettoIS.gruppo01.Exceptions.InsufficientNumbersException;
 import progettoIS.gruppo01.Exceptions.MathException;
 
@@ -13,41 +16,93 @@ public class MathExecuter {
         this.stackNumeri = stackNumeri;
     }
     
-    /*Metodo che preleva i primi due numeri dalla cima
-    dello stack e inserisce la loro somma*/
-    public void stackAdd() throws InsufficientNumbersException{
+    /*Metodo che preleva i primi due numeri dalla
+    cima dello stack e inserisce in esso la loro somma.
+    Può lanciare un’eccezione controllata di tipo InsufficientNumberException se 
+    lo stack ha meno di due numeri.
+    */
+    public void stackAdd() throws InsufficientNumbersException, EmptyStackException, FullStackException{
+        if(stackNumeri.getSize()<=1){
+              throw new InsufficientNumbersException("Insufficient Numbers Error");
+         }
+        ComplexNum primo=stackNumeri.remove(); //Prelevo il primo numero complesso dello stack
+        ComplexNum secondo=stackNumeri.remove(); //Prelevo il secondo numero complesso dello stack
+        stackNumeri.insert(somma(primo,secondo)); //Inserisco la somma dei due numeri nello stack
 
         
     }
     
     /*Metodo che preleva i primi due numeri dalla cima
-    dello stack e inserisce la loro sottrazione*/
-    public void stackSub()throws InsufficientNumbersException{
+    dello stack e inserisce in esso la loro sottrazione.
+    Può lanciare un’eccezione controllata di tipo InsufficientNumberException se 
+    lo stack ha meno di due numeri.
+    */
+    public void stackSub()throws InsufficientNumbersException, EmptyStackException, FullStackException{
+         if(stackNumeri.getSize()<=1){
+              throw new InsufficientNumbersException("Insufficient Numbers Error");
+         }
+        ComplexNum primo=stackNumeri.remove(); //Prelevo il primo numero complesso dello stack
+        ComplexNum secondo=stackNumeri.remove(); //Prelevo il secondo numero complesso dello stack
+        stackNumeri.insert(sottrazione(primo,secondo)); //Inserisco la sottrazione dei due numeri nello stack
+
         
     }
     
     /*Metodo che preleva i primi due numeri dalla cima
-    dello stack e inserisce la loro divisione*/
-    public void stackDiv()throws InsufficientNumbersException,MathException{
+    dello stack e inserisce in esso la loro divisione.
+    Può lanciare un’eccezione controllata di tipo InsufficientNumberException se 
+    lo stack ha meno di due numeri,e un’eccezione controllata di tipo MathException se si prova a dividere per 0.
+    */
+    public void stackDiv()throws InsufficientNumbersException,MathException, EmptyStackException, FullStackException{
+         if(stackNumeri.getSize()<=1){
+              throw new InsufficientNumbersException("Insufficient Numbers Error");
+         }
+        ComplexNum primo=stackNumeri.remove();//Prelevo il primo numero complesso dello stack
+        ComplexNum secondo=stackNumeri.remove();//Prelevo il secondo numero complesso dello stack
+        ComplexNum div=divisione(primo,secondo); //Eseguo l'operazione di divisione dei due numeri
+        if(div==null){
+            throw new MathException("Math Exception Error"); //Eccezione lanciata se il secondo numero è uguale a 0
+        }
+        stackNumeri.insert(div); //Inserisco la divisione dei due numeri nello stack
+
         
     }
     
-    /*Metodo che preleva i primi due numeri dalla cima
-    dello stack e inserisce la loro moltiplicazione*/
-    public void stackMul()throws InsufficientNumbersException{
+    /*
+    Metodo che preleva i primi due numeri dalla cima
+    dello stack e inserisce in esso la loro moltiplicazione.
+    Può lanciare un’eccezione controllata di tipo InsufficientNumberException se 
+    lo stack ha meno di due numeri.
+    */
+    public void stackMul()throws InsufficientNumbersException, EmptyStackException, FullStackException{
+         if(stackNumeri.getSize()<=1){
+              throw new InsufficientNumbersException("Insufficient Numbers Error");
+         }
+        ComplexNum primo=stackNumeri.remove(); //Prelevo il primo numero complesso dello stack
+        ComplexNum secondo=stackNumeri.remove(); //Prelevo il secondo numero complesso dello stack
+        stackNumeri.insert(moltiplicazione(primo,secondo)); //Inserisco la moltiplicazione dei due numeri nello stack
         
     }
     
     /*Metodo che preleva il numero in cima allo stack
-     e inserisce lo stesso numero ma invertito di segno*/
-    public void stackSignInv()throws EmptyStackException{
+     e inserisce in esso lo stesso numero ma invertito di segno.
+    Può lanciare un’eccezione controllata di tipo EmptyStackException se 
+    lo stack è vuoto.
+    */
+    public void stackSignInv()throws EmptyStackException, FullStackException{
+        ComplexNum num=stackNumeri.remove(); //Prelevo il numero complesso dello stack
+        stackNumeri.insert(invSegno(num)); //Inserisco il numero invertito di segno nello stack
         
     }
     
     /*Metodo che preleva il numero in cima allo stack
-     e inserisce la sua radice*/
-    public void stackSqrt()throws EmptyStackException{
-        
+     e inserisce in esso la sua radice.
+    Può lanciare un’eccezione controllata di tipo EmptyStackException se 
+    lo stack è vuoto.
+    */
+    public void stackSqrt()throws EmptyStackException, FullStackException{
+        ComplexNum num=stackNumeri.remove(); //Prelevo il numero complesso dello stack
+        stackNumeri.insert(sqrt(num)); //Inserisco la radice del numero nello stack
     }
    
     
--- src/main/java/progettoIS/gruppo01/Calculator/ButtonOp/StackCommander.java
+++ src/main/java/progettoIS/gruppo01/Calculator/ButtonOp/StackCommander.java
@@ -12,11 +12,14 @@ import progettoIS.gruppo01.Exceptions.InsufficientNumbersException;
 public class StackCommander {
     private ComplexStack stackNumeri;
 
+    
     public StackCommander(ComplexStack stackNumeri) {
         this.stackNumeri = stackNumeri;
     }
     
-    /*Metodo che svuota completamente lo stack*/
+    /*Metodo che svuota completamente lo stack.
+    Può lanciare un’eccezione controllata di tipo EmptyStackException se lo stack è vuoto.
+    */
     public void stackClear() throws EmptyStackException{
         for(int i=0;i<stackNumeri.getSize();i++){
             stackNumeri.remove();
@@ -24,67 +27,78 @@ public class StackCommander {
         
     }
     
-    /*Metodo che rimuove il numero in cima allo stack*/ 
+    /*Metodo che rimuove il numero in cima allo stack.
+    Può lanciare un’eccezione controllata di tipo EmptyStackException se lo stack è vuoto.
+    */ 
     public void stackDrop() throws EmptyStackException{
-        if(stackNumeri.getSize()==0){
-            throw new EmptyStackException("Empty Stack Error");
-        }
-        stackNumeri.remove();
+        
+        stackNumeri.remove(); 
 
     }
         
-    /*Metodo che duplica il numero presente in cima allo stack*/
+    /*Metodo che duplica il numero presente in cima allo stack
+    Può lanciare un’eccezione controllata di tipo FullStackException se lo stack è pieno,
+    e un’eccezione controllata di tipo EmptyStackException se lo stack è vuoto
+    */
      public void stackDup() throws FullStackException,EmptyStackException{
          if(stackNumeri.getSize()==0){
              throw new EmptyStackException("Empty Stack Error");
          }
-         stackNumeri.insert(stackNumeri.top());
+         stackNumeri.insert(stackNumeri.top()); 
 
     }
      
-     /*Metodo che inverte la posizione dei due numeri in cima allo stack*/
+     /*Metodo che inverte la posizione dei due numeri in cima allo stack.
+     Può lanciare un’eccezione controllata di tipo InsufficientNumbersException se lo stack ha meno di due numeri.
+     */
      public void stackSwap() throws InsufficientNumbersException, EmptyStackException, FullStackException{
          if(stackNumeri.getSize()<=1){
               throw new InsufficientNumbersException("Insufficient Numbers Error");
          }
-         ComplexNum primo=stackNumeri.remove();
-         ComplexNum secondo=stackNumeri.remove();
-          stackNumeri.insert(primo);
-         stackNumeri.insert(secondo);
+         ComplexNum primo=stackNumeri.remove(); //Prelievo del primo numero 
+         ComplexNum secondo=stackNumeri.remove(); //Prelievo del secondo numero 
+         stackNumeri.insert(primo); //Reinserimento del primo numero
+         stackNumeri.insert(secondo); //Reinserimento del secondo numero
          
 
     }
      
-     /*Metodo che duplica in cima allo stack il numero nella seconda posizione*/
+     /*Metodo che duplica in cima allo stack il numero nella seconda posizione
+     Può lanciare un’eccezione controllata di tipo InsufficientNumbersExceptionse lo stack ha meno di due numeri,
+     e un’eccezione controllata di tipo FullStackExceptionse lo stack è pieno
+     */
      public void stackOver() throws InsufficientNumbersException,FullStackException, EmptyStackException{
           if(stackNumeri.getSize()<=1){
               throw new InsufficientNumbersException("Insufficient Numbers Error");
          }
-         ComplexNum primo=stackNumeri.remove();
-         ComplexNum secondo=stackNumeri.top();
-         stackNumeri.insert(primo);
-         stackNumeri.insert(secondo);
+         ComplexNum primo=stackNumeri.remove(); //Prelievo del primo numero 
+         ComplexNum secondo=stackNumeri.top(); //Copia del secondo numero
+         stackNumeri.insert(primo); //Reinserimento del primo numero
+         stackNumeri.insert(secondo); //Inserimento della copia del secondo numero
 
     }
     
      /*Metodo che restituisce un vettore di stringhe contenente
-     i primi'num' numeri dalla cima dello dello stack*/
+     i primi 'num' numeri dalla cima dello dello stack.
+     Se lo stack ha meno di ‘num’ numeri, il vettore conterrà tutti i numeri presenti nello stack.
+     Se lo stack è vuoto, restituisce Null.
+     */
      public String[] getContent(int num) throws EmptyStackException, FullStackException{
          if(stackNumeri.getSize()==0){
              return null;
          }
-         if(num>stackNumeri.getSize()){
-             num=stackNumeri.getSize();
+         if(num>stackNumeri.getSize()){ //Controllo che 'num' non sia maggiore rispetto al numero di valori attualmente contenuti nello stack 
+             num=stackNumeri.getSize(); //Controllo effettuato per evitare inutili rimozioni dallo stack che causerebbero EmptyStackException
          }
-         ComplexNum t[] = new ComplexNum[num];
-         String v[] = new String[num];
+         ComplexNum t[] = new ComplexNum[num]; //Vettore di ComplexNum per contenere temporaneamente i numeri rimossi dallo stack
+         String v[] = new String[num]; //Vettore di stringhe in cui inserire i numeri,espressi nel formato restituito dal loro metodo toString() 
          
          for(int i=0;i<num;i++){
-             t[i]=stackNumeri.remove();
-             v[i]=t[i].toString();
+             t[i]=stackNumeri.remove(); //Rimozione temporanea dei numeri dallo stack
+             v[i]=t[i].toString(); //Inserimento, nel vettore di stringhe, dei numeri espressi nel formato restituito dal loro metodo toString()
          }
           for(int i=num;i>0;i--){
-             stackNumeri.insert(t[i]);
+             stackNumeri.insert(t[i]); //Reinserimento dei numeri nello stack
          }
          
          
