b11fb05838ebc47618884721f562729c13ed6e89
attil <attil@LAPTOP-B5H566RC.homenet.telecomitalia.it>
2023-12-09 20:23:27 +0100

    Implementazione parziale del test di InsertionManagement

---
+++ src/test/java/progettoIS/gruppo01/Calculator/Management/VarManagementTest.java
@@ -0,0 +1,48 @@
+package progettoIS.gruppo01.Calculator.Management;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author attil
+ */
+public class VarManagementTest {
+    
+    public VarManagementTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class VarManagement.
+     */
+    @Test
+    public void testExecute() throws Exception {
+        System.out.println("execute");
+        String input = "";
+        VarManagement instance = null;
+        instance.execute(input);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
