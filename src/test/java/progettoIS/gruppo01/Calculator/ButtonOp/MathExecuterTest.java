aa1bf50b68b0c8b5d666d6c1e96f122b5ed680c5
gianl <gianl@172.19.154.83>
2023-12-11 12:12:12 +0100

    Implementazione test case delle calssi MathExecuter e StackCommander nel package progettoIS.gruppo01.Calculator.ButtonOP

---
+++ src/test/java/progettoIS/gruppo01/Calculator/ButtonOp/MathExecuterTest.java
@@ -0,0 +1,264 @@
+package progettoIS.gruppo01.Calculator.ButtonOp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import progettoIS.gruppo01.Calculator.Complex.ComplexNum;
+import progettoIS.gruppo01.Calculator.Complex.MathOperations;
+import static progettoIS.gruppo01.Calculator.Complex.MathOperations.somma;
+import progettoIS.gruppo01.Calculator.Structures.ComplexStack;
+import progettoIS.gruppo01.Exceptions.EmptyStackException;
+import progettoIS.gruppo01.Exceptions.FullStackException;
+import progettoIS.gruppo01.Exceptions.InsufficientNumbersException;
+import progettoIS.gruppo01.Exceptions.MathException;
+
+public class MathExecuterTest {
+    
+    private ComplexStack stackNumeri;
+    private final int MAX_CAPACITY = 10;
+    private MathExecuter instance;
+    
+    @BeforeEach
+    public void setUp() {
+        
+        stackNumeri = new ComplexStack(MAX_CAPACITY);
+        instance = new MathExecuter(stackNumeri);
+        
+    }
+
+    /**
+     * Test of stackAdd method, of class MathExecuter.
+     */
+    
+    
+    //Test di stackAdd con stack non vuoto e entrambi inumeri hanno parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackAdd1() throws FullStackException, InsufficientNumbersException, EmptyStackException {
+        
+        System.out.println("Test stackAdd : caso ordinario");
+        
+        ComplexNum num1 = new ComplexNum(2.0 , -37.2);
+        ComplexNum num2 = new ComplexNum(-9.8 , 5);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackAdd();
+        
+        assertEquals(MathOperations.somma(num1,num2),stackNumeri.top());
+        
+    }
+
+    //Test di stackAdd con stack vuoto.
+    
+    @Test
+    public void testStackAdd2() {
+        
+        System.out.println("Test stackAdd : numeri insufficienti nello stack");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackAdd();
+        });
+        
+    }
+    
+    
+
+    /**
+     * Test of stackSub method, of class MathExecuter.
+     */
+    
+    //Test di stackSub con stack non vuoto e entrambi inumeri hanno parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackSub1() throws InsufficientNumbersException, EmptyStackException, FullStackException {
+        
+        System.out.println("Test stackSub : caso ordinario");
+        
+        ComplexNum num1 = new ComplexNum(-5.9 , 46);
+        ComplexNum num2 = new ComplexNum(36.0 , 23.2);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackSub();
+        
+        assertEquals(MathOperations.sottrazione(num1,num2),stackNumeri.top()); //invertendo l'ordine il test fallisce
+        
+    }
+    
+    //Test di stackSub con stack vuoto.
+    
+    @Test
+    public void testStackSub2() {
+        
+        System.out.println("Test stackSub : numeri insufficienti nello stack");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackSub();
+        });
+        
+    }
+
+    /**
+     * Test of stackDiv method, of class MathExecuter.
+     */
+   
+    //Test di stackDiv con stack non vuoto e entrambi inumeri hanno parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackDiv1() throws InsufficientNumbersException, MathException, EmptyStackException, FullStackException {
+        
+        System.out.println("Test stackDiv: caso ordinario");
+        
+        ComplexNum num1 = new ComplexNum(17.5 , -15.0);
+        ComplexNum num2 = new ComplexNum(23 , 9.17);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackDiv();
+        
+        assertEquals(MathOperations.divisione(num1,num2),stackNumeri.top()); //invertendo l'ordine il test fallisce
+        
+    }
+    
+    //Test di stackDiv con stack vuoto.
+    
+    @Test
+    public void testStackDiv2() {
+        
+        System.out.println("Test stackDiv: numeri insufficenti nello stack");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackDiv();
+        });
+        
+    }
+    
+    //Test di stackDiv nel caso in cui il dividendo sia 0.
+    
+    @Test
+    public void testStackDiv3() throws FullStackException, InsufficientNumbersException, MathException {
+        
+        System.out.println("Test stackDiv: divisione per zero");
+        
+        ComplexNum num1 = new ComplexNum(57.2 , -34.8);
+        ComplexNum num2 = new ComplexNum(0 , 0);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        assertThrows(MathException.class,() -> {
+            instance.stackDiv();
+        });
+        
+    }
+
+    /**
+     * Test of stackMul method, of class MathExecuter.
+     */
+  
+    //Test di stackMul con stack non vuoto e entrambi inumeri hanno parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackMul1() throws InsufficientNumbersException, EmptyStackException, FullStackException {
+       
+        System.out.println("Test stackMul: caso ordinario");
+        
+        ComplexNum num1 = new ComplexNum(-45.1, 22.2);
+        ComplexNum num2 = new ComplexNum(21 , 10.9);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackMul();
+        
+        assertEquals(MathOperations.moltiplicazione(num2,num1),stackNumeri.top());
+
+        
+    }
+    
+    //Test di stackMul con stack vuoto.
+    
+    @Test
+    public void testStackMul2() {
+       
+        System.out.println("Test stackMul: nueri insufficienti nello stack");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackMul();
+        });
+        
+    }
+
+    /**
+     * Test of stackSignInv method, of class MathExecuter.
+     */
+ 
+    //Test di stackSignInv con stack non vuoto e e il numero ha parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackSignInv1() throws EmptyStackException, FullStackException {
+        
+        System.out.println("Test stackSignInv: caso ordinario"); 
+        
+        ComplexNum num1 = new ComplexNum(-11.7, 33);
+        
+        stackNumeri.insert(num1);
+        
+        instance.stackSignInv();
+        
+        assertEquals(MathOperations.invSegno(num1),stackNumeri.top());
+        
+    }
+    
+    //Test di stackSignInv con stack vuoto.
+    
+    @Test
+    public void testStackSignInv2() {
+        
+        System.out.println("Test stackSignInv: numeri insufficienti nello stack");
+        
+        assertThrows(EmptyStackException.class,() -> {
+            instance.stackSignInv();
+        });
+        
+    }
+
+    /**
+     * Test of stackSqrt method, of class MathExecuter.
+     */
+ 
+    //Test di stackSqrt con stack non vuoto e il numero ha parte reale e immaginaria non nulle.
+    
+    @Test
+    public void testStackSqrt1() throws EmptyStackException, FullStackException {
+        
+        System.out.println("Test stackSqrt: caso ordinario");
+        
+        ComplexNum num1 = new ComplexNum(7.77, -32.5);
+        
+        stackNumeri.insert(num1);
+        
+        instance.stackSqrt();
+        
+        assertEquals(MathOperations.sqrt(num1),stackNumeri.top());
+       
+    }
+    
+    //Test di stackSqrt con stack vuoto.
+    
+    @Test
+    public void testStackSqrt2() {
+        
+        System.out.println("Test stackSqrt: numeri insufficinenti nello stack");
+        
+        assertThrows(EmptyStackException.class,() -> {
+            instance.stackSqrt();
+        });
+        
+    }
+
+}
---
+++ src/test/java/progettoIS/gruppo01/Calculator/ButtonOp/StackCommanderTest.java
@@ -0,0 +1,305 @@
+package progettoIS.gruppo01.Calculator.ButtonOp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import progettoIS.gruppo01.Calculator.Complex.ComplexNum;
+import progettoIS.gruppo01.Calculator.Structures.ComplexStack;
+import progettoIS.gruppo01.Exceptions.EmptyStackException;
+import progettoIS.gruppo01.Exceptions.FullStackException;
+import progettoIS.gruppo01.Exceptions.InsufficientNumbersException;
+
+public class StackCommanderTest {
+    
+    private ComplexStack stackNumeri;
+    private final int MAX_CAPACITY = 50;
+    private StackCommander instance;
+    
+    @BeforeEach
+    public void setUp() {
+        
+        stackNumeri = new ComplexStack(MAX_CAPACITY);
+        instance = new StackCommander(stackNumeri);
+        
+    }
+
+    /**
+     * Test of stackClear method, of class StackCommander.
+     */
+    
+    //Test stackClear quando lo stack ha elementi all'interno.
+    
+    @Test
+    public void testStackClear1() throws FullStackException {
+        
+        System.out.println("Test stackClear: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(13.1 , -7.2);
+        ComplexNum num2 = new ComplexNum(30.2 , 55);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackClear();
+        
+        assertEquals(0,stackNumeri.getSize());
+        
+    }
+    
+    //Test stackClear quando lo stack è vuoto.
+    
+    @Test
+    public void testStackClear2() {
+        
+        System.out.println("Test stackClear: stack vuoto");
+        
+        assertEquals(0,stackNumeri.getSize());
+        
+    }
+
+    
+    /**
+     * Test of stackDrop method, of class StackCommander.
+     */
+    
+    //Test StackDrop quando lo stack ha elementi all'interno.
+    
+    @Test
+    public void testStackDrop1() throws FullStackException, EmptyStackException {
+        
+        System.out.println("Test stackDrop: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(4.9 , 26.9);
+        ComplexNum num2 = new ComplexNum(37.6 , -9.9);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackDrop();
+        
+        assertEquals(num1,stackNumeri.top());
+        
+    }
+    
+    //Test stackDrop quando lo stack è vuoto.
+    
+    @Test
+    public void testStackDrop2() {
+        
+        System.out.println("Test stackDrop: stack vuoto");
+        
+        assertThrows(EmptyStackException.class,() -> {
+            instance.stackDrop();
+        });
+
+    }
+
+    /**
+     * Test of stackDup method, of class StackCommander.
+     */
+   
+    //Test stackDup quando lo stack ha elementi all'interno.
+    
+    @Test
+    public void testStackDup1() throws FullStackException, EmptyStackException {
+        
+        System.out.println("Test stackDup: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(88.8 , -43);
+        
+        stackNumeri.insert(num1);
+        
+        instance.stackDup();
+        
+        assertEquals(num1,stackNumeri.top());
+          
+    }
+    
+    
+    //Test stackDup quando lo stack è vuoto.
+    
+    @Test
+    public void testStackDup2() {
+        
+        System.out.println("Test stackDup: stack vuoto");
+        
+        assertThrows(EmptyStackException.class,() -> {
+            instance.stackDup(); //stackClear non tira EmptyStackException
+        });
+        
+    }
+    
+    //Test stackDup quando lo stack è pieno. 
+    
+    @Test
+    public void testStackDup3() throws Exception {
+        
+        System.out.println("Test stackDup: stack pieno");
+        
+        ComplexNum num1 = new ComplexNum(32.21 , -11);
+        
+        for(int i=0; i<MAX_CAPACITY; i++){
+            
+            stackNumeri.insert(num1);
+            
+        }
+        
+        assertThrows(FullStackException.class,() -> {
+            instance.stackDup();
+        });
+          
+    }
+
+    /**
+     * Test of stackSwap method, of class StackCommander.
+     */
+    
+    //Test stackSwap quando lo stack ha elementi all'interno.
+    
+    @Test
+    public void testStackSwap1() throws FullStackException, InsufficientNumbersException, EmptyStackException {
+        
+        System.out.println("Test stackSwap: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(64 , -232.234);
+        ComplexNum num2 = new ComplexNum(-32.1 , -12);
+
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackSwap();
+        
+        assertEquals(num1,stackNumeri.top());
+         
+    }
+    
+    //Test stackSwap quando lo stack è vuoto.
+    
+    @Test
+    public void testStackSwap2() {
+        
+        System.out.println("Test stackSwap: stack vuoto");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackSwap(); 
+        });
+         
+    }
+
+    /**
+     * Test of stackOver method, of class StackCommander.
+     */
+
+    //Test stackOver quando lo stack non è vuoto.
+    
+    @Test
+    public void testStackOver1() throws FullStackException, InsufficientNumbersException, EmptyStackException {
+        
+        System.out.println("Test stackOver: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(56 , 21.2);
+        ComplexNum num2 = new ComplexNum(99.0 , 10);
+
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        instance.stackOver();
+        
+        assertEquals(num1,stackNumeri.top());
+        
+    }
+       
+    //Test stackOver quando lo stack è vuoto.
+    
+    @Test
+    public void testStackOver2() {
+        
+        System.out.println("Test stackOver: stack è vuoto");
+        
+        assertThrows(InsufficientNumbersException.class,() -> {
+            instance.stackOver(); 
+        });
+        
+    }
+    
+    //Test stackOver quando lo stack è pieno.
+    
+    @Test
+    public void testStackOver3() throws FullStackException, InsufficientNumbersException, EmptyStackException {
+        
+        System.out.println("Test stackOver: stack non vuoto");
+        
+        ComplexNum num1 = new ComplexNum(11 , 12.23);
+        
+        for(int i = 0; i < MAX_CAPACITY; i++){
+        
+            stackNumeri.insert(num1);
+        
+        }
+        
+        assertThrows(FullStackException.class,() -> {
+            instance.stackOver();
+        });
+        
+    }
+
+    /**
+     * Test of getContent method, of class StackCommander.
+     */
+
+    //Test getContent quando nello stack sono presenti lo stesso numero degli elementi richiesti.
+    
+    @Test
+    public void testGetContent1() throws FullStackException {
+        
+        System.out.println("Test getContent: stack con stesso numero di elemtni quanto sono quelli richiesti");
+        
+        ComplexNum num1 = new ComplexNum(5 , -13.9);
+        ComplexNum num2 = new ComplexNum(48 , 8.2);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        int num = 2;
+        String[] expResult = {"5 - 13.9j","48 + 8.2j"};
+        String[] result = instance.getContent(num);
+        assertArrayEquals(expResult, result);
+        
+    }
+    
+    //Test getContent quando nello stack sono presenti meno numeri di quelli richiesti.
+    
+    @Test
+    public void testGetContent2() throws FullStackException {
+        
+        System.out.println("Test getContent: stack con meno elementi di quelli richiesti");
+        
+        ComplexNum num1 = new ComplexNum(-45.9 , -52.3);
+        ComplexNum num2 = new ComplexNum(11.8 , 113);
+        
+        stackNumeri.insert(num1);
+        stackNumeri.insert(num2);
+        
+        int num = 3;
+        String[] expResult = {"-45.9 - 52.3j","11.8 + 113j"};
+        String[] result = instance.getContent(num);
+        assertArrayEquals(expResult, result);
+
+    }
+    
+    //Test getContent quando lo stack è vuoto.
+
+    @Test
+    public void testGetContent3() {
+        
+        System.out.println("Test getContent: stack vuoto");
+        
+        int num = 10;
+        String[] expResult = null;
+        String[] result = instance.getContent(num);
+        assertArrayEquals(expResult, result);
+        
+        
+    }
+
+}
